acec258b228bb83c2e96772844024db786568656
staehle <jacob.staehle@my.und.edu>
2015-10-07 12:17:51 -0500

    C++11 thread implementation

--- splitter.cpp
+++ splitter.cpp
@@ -10,6 +10,7 @@ using namespace std;
 #define BP_SPLIT_SIZE 128;
 
 string fastq_orig;
+int workload;
 
 int splicer(int filenum) {
 	ifstream fin;
@@ -21,19 +22,42 @@ int splicer(int filenum) {
 	while(!fin.eof()) {
 		
 	}
+	
+	workload--;
 }
 
 int main(int argc, char **argv) {
 	cout << "ADNA Split" << endl;
 	if (argc < 2) {
 		cout << "Error: Not enough arguments" << endl;
-		cout << "Usage: " << argv[0] << " <input_fastq_file> <options>" << endl;
+		cout << "Usage: " << argv[0] << " <input_fastq_file> <OPTIONS> -t <threads> -o <output> " << endl;
 		return -1;
 	}
+	fastq_orig = argv[1];
+	fastq_size = 2048; // calculate each bp set: number of lines in the fastq file / 4 (each set)
 	
-	fastq_orig = argv[1]; 
-
-	// Split FASTQ File into equal segments
-
+	int numthreads = argv[4];
+	thread threadpool[numthreads];
+	
+	workload = fastq_size / BP_SPLIT_SIZE;
+	
+	for (int t=0; t < numthreads; t++) {
+		threadpool[t](splicer, n);
+	}
+	
+	for (int t=0; t < numthreads; t++) {
+		threadpool[t].join();
+	}
+	cout << "The threadpool has terminated." << endl;
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
