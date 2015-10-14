f435a75dcc08b591caa8968b9c509b7e92f2a628
staehle <jacob.staehle@my.und.edu>
2015-10-13 19:08:41 -0500

    Combined functions

--- splitter.cpp
+++ splitter.cpp
@@ -14,10 +14,11 @@ string fastq_read1;
 string fastq_read2;
 int numthreads;
 int dead;
+int readnum;
 
-void splitter(int threadnum) { // Read 1: Split entire FASTQ into a separate file per read
-	string report = "Starting splitter thread " + to_string(threadnum);
-	cerr << report << endl;
+void dosplit(int threadnum) {
+	string report = "Read #" + to_string(readnum) + ": Starting splitter thread " + to_string(threadnum) + "\n";
+	cerr << report;
 	ifstream fin;
 	ofstream curof;
 	string line;
@@ -37,7 +38,8 @@ void splitter(int threadnum) { // Read 1: Split entire FASTQ into a separate fil
 		if (line.length() < 4) break;
 		string head = "./raw/"+line.substr(5,37)+"_X_"+line.substr(45);
 		replace(head.begin(), head.end(), ':', '_');
-		curof.open(head, ios::out);
+		if (readnum==1) curof.open(head, ios::out);
+		else if (readnum==2) curof.open(head, ios::app);
 		curof<<line<<endl;
 		for (int i=0; i<3; i++) {
 			getline(fin, line);
@@ -49,48 +51,8 @@ void splitter(int threadnum) { // Read 1: Split entire FASTQ into a separate fil
 			fin.ignore(numeric_limits<streamsize>::max(),'\n');
 		}
 	}
-	report = "Thread " + to_string(threadnum) + " has completed";
-	cerr << report << endl;
-	dead++;
-	fin.close();
-}
-
-void splicer(int threadnum) { // Read 2: Splice read 2 into existing split files from read 1
-	string report = "Starting splicer thread " + to_string(threadnum);
-	cerr << report << endl;
-	ifstream fin;
-	ofstream curof;
-	string line;
-	fin.open(fastq_read2, ios::in);
-	if (!fin.is_open()) {
-		cerr << "Error: Cannot open file" << endl;
-		exit(1);
-	}
-	int roundnum = 0;
-	// where should i start? 
-	// start of each is (threadnum*4)+(numthreads*4*roundnum);
-	for(int i=0; i<(threadnum*4); ++i) {
-		fin.ignore(numeric_limits<streamsize>::max(),'\n');
-	}	
-	while(!fin.eof()) {
-		getline(fin, line);
-		if (line.length() < 4) break;
-		string head = "./raw/"+line.substr(5,37)+"_X_"+line.substr(45);
-		replace(head.begin(), head.end(), ':', '_');
-		curof.open(head, ios::app);
-		curof<<line<<endl;
-		for (int i=0; i<3; i++) {
-			getline(fin, line);
-			curof<<line<<endl;
-		}
-		curof.close();
-		roundnum++;
-		for(int i=0; i<(numthreads*4)-4; ++i) {
-			fin.ignore(numeric_limits<streamsize>::max(),'\n');
-		}
-	}
-	report = "Thread " + to_string(threadnum) + " has completed";
-	cerr << report << endl;
+	report = "Read #" + to_string(readnum) + ": Thread " + to_string(threadnum) + " has completed\n";
+	cerr << report;
 	dead++;
 	fin.close();
 }
@@ -112,11 +74,12 @@ int main(int argc, char **argv) {
 	}
 	
 	/* Splitter for read 1 */
+	readnum = 1;
 	dead = 0;
 	cerr << "Splitting read 1 with thread pool of "<<numthreads<<" threads"<<endl;
 	thread threadpool[numthreads];
 	for (int t=0; t < numthreads; t++) {
-		threadpool[t] = thread(splitter, t);
+		threadpool[t] = thread(dosplit, t);
 	}
 	for (int t=0; t < numthreads; t++) {
 		threadpool[t].join();
@@ -125,18 +88,17 @@ int main(int argc, char **argv) {
 	cerr << "The splitter pool for read 1 has terminated." << endl;
 	
 	/* Splicer for read 2 */
+	readnum = 2;
 	dead = 0;
 	cerr << "Opening read 2 and adding to split files with thread pool of "<<numthreads<<" threads"<<endl;
-	
 	for (int t=0; t < numthreads; t++) {
-		threadpool[t] = thread(splicer, t);
+		threadpool[t] = thread(dosplit, t);
 	}
 	for (int t=0; t < numthreads; t++) {
 		threadpool[t].join();
 	}
 	while (dead<numthreads); // poor man's thread block
 	cerr << "The splicer pool for read 2 has terminated." << endl;
-	
 	
 	cerr << "ADNA-Split has completed!"<<endl;
 	
