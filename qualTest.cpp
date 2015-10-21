8363733de8267b5f669c895fcb93edfb80967673
brandon-vovk <brandon.vovk@my.und.edu>
2015-10-21 01:26:38 -0500

    Now used as a test file for the ReadPair class
    I also committed a new object class ReadPair and this file is now just a development file for testing it.

--- qualTest.cpp
+++ qualTest.cpp
@@ -1,65 +1,28 @@
+//#include "qualTest.h"
 // Example program
 #include <iostream>
 #include <string>
+#include <stdio.h>
+#include "ReadPair.h"
 using namespace std;
 
-
-// returns 1 if string passed qual test: < 10 bases with < 15 qual score
-// symbol ! = 0, symbol 0 = 15 <-Threshold
-int qualPass(string tString)
-{
-    int qual = -1;
-	int failCtr = 0;
-	for(char& c : tString)
-	{
-	    qual = int(c)-33;
-	    printf("%c : %i\n", c, qual);
-		if(int(c)-33 < 15)
-		{
-			failCtr+=1;
-			printf("%i\n", failCtr);
-		}
-	}
-	printf("FAILURES : %i\n\n",failCtr);
-	if (failCtr < 10)
-	{
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
-}
-
-//check for a leading T or 2 trailing T's and replace the corresponding quality scores to 2 (#)
-//tStrip takes the nd and 4th lines of a read and returns an updated qual string
-string tStrip(string seq, string qual)
-{
-    if(seq.at(0) == 'T')
-    {
-        printf("found a T\n");
-        qual[0] = '#';
-    }
-    if((seq.at(seq.length() - 1) == 'T') && (seq.at(seq.length() - 2) == 'T'))
-    {
-        printf("found ending T's\n");
-        qual[qual.length() - 1] = '#';
-        qual[qual.length() - 2] = '#';
-    }
-    
-    return qual;
-}
+class ReadPair;
 
 //All the print statements in this file are currently for debuggung only, will not be in final software
 int main()
 {
 	//sequence to test the T search and qual replacements
-	string x = "TGCCGGAGTTTCGCGCCGCGGGCGCTTCGCAGGCGATAACCTGAAACTTGANGGAAGAGCNNCCGCAGTTGACCACCAGTACATTCAGATCGGAAGAGCACACGTCTGAACTCCAGTCACCAGTT";
-	string s = "BC@BCGGGGGGGGGGGGGGGGGGGGGGGGGFGGGGGGGGGGC@FFGFGGGG#==E<FFGG##=<:EGGEGACEC</0<000800000008000<0;0000;000/8///69.6//9/...9////";
-	s = tStrip(x,s);
-	qualPass(s);
-	string t = "BBCCCGGGGGGGGGGGGGGGGGGEGGGGGGGGGGCFGGG@DGBCGGGGGGG#;;CFFCEG##=::EGG@EGGGGGGGGGCGGGG..@GGGGCGGGGGGCG.8.:DEDG.9.CEGG<DDGD<CDG/";
-	qualPass(t);
-	string v = "CCCCCGGGGGGGDGGGGGGGGGGGGGGGGGGGGGEGGGGGGGGGGGGFGGG##=##==EF###===FGGGGFFGGGGGGGG0FDGGGGG.CG@GGBGGGGGGGGGGGGGGGGGGGGG/EB@G/8/";
-	qualPass(v);
+	string xR = "TGCCGGAGTTTCGCGCCGCGGGCGCTTCGCAGGCGATAACCTGAAACTTGANGGAAGAGCNNCCGCAGTTGACCACCAGTACATTCAGATCGGAAGAGCACACGTCTGAACTCCAGTCACCAGTC";
+	string xQ = "BC@BCGGGGGGGGGGGGGGGGGGGGGGGGGFGGGGGGGGGGC@FFGFGGGG#==E<FFGG##=<:EGGEGACEC</0<000800000008000<0;0000;000/8///69.6//9/...9////";
+	string yR = "TCCAGTCACCAGTCGATAAACGCCCTGCCAAACGGCGCAAGGATGCGCCGCACGAAAATCAAACGCGCAAGTGCTTGATTTTCGGAGCGCGCCGTGGGCGAAGCGGAGCTCTCGCGGATGCACTT";
+	string yQ = "CCCCCGGGGGGGDGGGGGGGGGGGGGGGGGGGGGEGGGGGGGGGGGGFGGG##=##==EF###===FGGGGFFGGGGGGGG0FDGGGGG.CG@GGBGGGGGGGGGGGGGGGGGGGGG/EB@G/8/";
+	ReadPair myRead(xR,xQ,yR,yQ);
+	myRead.tStrip();
+	myRead.qualPass();
+	printf("\nfound %i as largest section of overlap over 11 bases\n",myRead.oCheck());
+
+	//qualPass(s);
+	//qualPass(t);
+	//qualPass(v);
+	return 0;
 }
