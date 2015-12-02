3a484bc86d949b2968fc5fddcbd18f1cfe3424c4
brandon-vovk <brandon.vovk@my.und.edu>
2015-12-02 02:22:53 -0600

    Update ReadPair.hpp

--- ReadPair.hpp
+++ ReadPair.hpp
@@ -2,10 +2,12 @@
 
 class ReadPair
 {
-	std::string read1, read2, qual1, qual2;
+	std::string read1, read2, qual1, qual2, fRead;
 public:
 	ReadPair(std::string, std::string,std::string, std::string);
-	void qualPass();
+	int qualPass();
 	void tStrip();
 	int oCheck();
+	int aRemove();
+	void Compile();
 };
