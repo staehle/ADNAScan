3336194e9c3c3df8b55056f53edbe2ad94f1b224
brandon-vovk <brandon.vovk@my.und.edu>
2015-12-09 20:53:52 -0600

    Update ReadPair.hpp

--- ReadPair.hpp
+++ ReadPair.hpp
@@ -1,22 +1,21 @@
-#ifndef READPAIR_H
-#define READPAIR_H
-
 #include <string>
+#include <vector>
 
 class ReadPair
 {
-	std::string read1, read2, qual1, qual2, fRead;
+	std::string ID1, ID2, read1, read2, qual1, qual2, fRead, fQual;
+	int tNum;
+	std::vector <std::string> PrintLongestArray;
+	//char fRead;
 public:
-	ReadPair(std::string, std::string);
-	ReadPair(std::string, std::string, std::string, std::string);
-	
-	void addR2(std::string, std::string);
-	
+	ReadPair(std::string, std::string, std::string,std::string, std::string,std::string , int);
+	ReadPair(std::string, std::string, std::string, int);
+	void addR2(std::string, std::string, std::string);
 	int qualPass();
 	void tStrip();
 	int oCheck();
 	int aRemove();
+	void passOutFile();
+	void failOutFile();
 	void Compile();
 };
-
-#endif
