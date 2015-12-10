272baaebba2d9d146c8bc5ba3733c3fd15775103
staehle <jacob.staehle@my.und.edu>
2015-12-09 23:37:39 -0600

    updated bash

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-mkdir -p results
-rm -rf ./results/*
 if [ "$1" == "" ] || [ "$2" == "" ] || [ "$3" == "" ]; then
 	echo "Usage: $0 <num_processes> <fastq1> <fastq2>"
 	echo
 	exit 1
 fi
-
-mpirun -np $1 $2 $3
+mkdir -p results
+rm -rf ./results/*
+mpirun -np $1 ./bin/adna $2 $3
