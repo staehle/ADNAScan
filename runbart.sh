8ce3e15dc22f0a8a948a530ed05d2a77c291a1f8
Jake Staehle <jacob.staehle@my.und.edu>
2016-01-13 11:03:32 -0600

    Create runbart.sh

---
+++ runbart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ "$1" == "" ] || [ "$2" == "" ] || [ "$3" == "" ]; then
+	echo "Usage: $0 <num_processes> <fastq1> <fastq2>"
+	echo
+	exit 1
+fi
+mkdir -p results
+rm -rf ./results/*
+/usr/local/gcc-5.3.0/openmpi/bin/mpirun -np $1 ./bin/adna $2 $3
