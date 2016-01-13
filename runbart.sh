d0f7e2832fbf8b29979115cb7bb2c0ea1ac25713
Jake Staehle <jacob.staehle@my.und.edu>
2016-01-13 12:43:29 -0600

    Update runbart.sh

--- runbart.sh
+++ runbart.sh
@@ -4,6 +4,8 @@ if [ "$1" == "" ] || [ "$2" == "" ] || [ "$3" == "" ]; then
 	echo
 	exit 1
 fi
+LD_LIBRARY_PATH=/usr/local/gcc-5.3.0/lib64/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH
 mkdir -p results
 rm -rf ./results/*
 /usr/local/gcc-5.3.0/openmpi/bin/mpirun -np $1 ./bin/adna $2 $3
