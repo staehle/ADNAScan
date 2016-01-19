99eed31843d7ec5c10a5ef06ce84965784d40d1d
brandon-vovk <brandon.vovk@my.und.edu>
2016-01-19 01:00:28 -0600

    Update runbart.sh

--- runbart.sh
+++ runbart.sh
@@ -17,36 +17,48 @@ id=${id::${#id}-5}
 id=${id##*/}
 #id=${`sed 's/.\{5\}$//' <<< "$2"`}
 id+="$DATE"
-r1P=$id"_1Pass.fastq"
-r2p=$id"_2Pass.fastq"
-r1F=$id"_1Fail.fastq"
-r2F=$id"_2Fail.fastq"
-sRP=$id"_singlePass.fastq"
-sRF=$id"_singleFail.fastq"
+r1P=$id"_r1Pass.fastq"
+r2p=$id"_r2Pass.fastq"
+r1F=$id"_r1Fail.fastq"
+r2F=$id"_r2Fail.fastq"
+sRP=$id"_singleReadPass.fastq"
+sRF=$id"_singleReadFail.fastq"
 
+touch $r1P
+touch $r2P
+touch $r1F
+touch $r2F
+touch $sRP
+touch $sRF
 
 for i in `ls | grep -E "*read1Pass*"` ; do
 	cat $i >> $r1P
+	rm $i
 done
 
 echo $r1P
 
 for i in `ls | grep -E "*read2Pass*"` ; do
 	cat $i >> $r2P
+	rm $i
 done
 
 for i in `ls | grep -E "*read1Fail*"` ; do
 	cat $i >> $r1F
+	rm $i
 done
 
 for i in `ls | grep -E "*read2Fail*"` ; do
 	cat $i >> $r2F
+	rm $i
 done
 
 for i in `ls | grep -E "*singleReadPass*"` ; do
 	cat $i >> $sRP
+	rm $i
 done
 
 for i in `ls | grep -E "*singleReadFail*"` ; do
 	cat $i >> $sRF
+	rm $i
 done
