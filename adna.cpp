c11a47adebe304d75e70f4274339c64d6ca20183
brandon-vovk <brandon.vovk@my.und.edu>
2016-01-19 16:54:11 -0600

    Update adna.cpp

--- adna.cpp
+++ adna.cpp
@@ -231,7 +231,7 @@ int main(int argc, char **argv) {
    ofstream oFile3;
    oFile3.open(bFileName, std::ios::app);
    oFile3 << "Number of reads (halves) from process " << my_rank << " that were below quality threshold: " << badReads << "\n";
-   oFile3 << "Good (halves): " << (2 * readsassigned - badReads - merged) << "\n";
+   oFile3 << "Good (halves): " << (2 * readsassigned - badReads - merges) << "\n";
    oFile3.close();
 
     return 0;
