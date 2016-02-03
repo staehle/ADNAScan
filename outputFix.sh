59262efbeefd29a27a8190bd6cce51505dcec020
brandon-vovk <brandon.vovk@my.und.edu>
2016-02-03 05:37:28 -0600

    Init
    Going to be used for quick fix of our output files

---
+++ outputFix.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -ne 1] ; then
+  echo Must have argument of path to results folder to fix
+  exit
+fi
+
+curD=`pwd`
+idDir=$1
+
+cd idDir
+
+ctr=0
+
+while read line ; do
+  if (( $ctr % 4 == 0 )) ; then
+    line="@"$line
+  fi
+  echo $line >> temp_UD.fastq
+done <mergeCount.temp
+
+cd $curD
